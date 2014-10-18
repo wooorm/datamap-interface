6aaecb08b3324f1add5e43cd3041562712c9c7f0
wooorm <tituswormer@gmail.com>
2014-10-18 16:39:46 +0200

    Refactor `index.js`

--- index.js
+++ index.js
@@ -1,90 +1,200 @@
 'use strict';
 
-var datamapInterfacePrototype, isOwnProperty;
+/**
+ * Cache.
+ */
+
+var isOwnProperty;
 
 isOwnProperty = Object.prototype.hasOwnProperty;
 
+/**
+ * An interface for a map of items.
+ *
+ * @constructor
+ * @param {Object.<string, *>} values
+ */
+
 function DatamapInterface(values) {
     this.map = {};
+
     this.add(values);
 }
 
-datamapInterfacePrototype = DatamapInterface.prototype;
+/**
+ * Detect if a key is defined on an object.
+ *
+ * @param {Object.<string, *>} object
+ * @param {string} key
+ * @return {boolean}
+ */
 
-function has(object, property) {
-    return isOwnProperty.call(object, property) &&
-        object[property] !== undefined;
+function has(object, key) {
+    return isOwnProperty.call(object, key) && object[key] !== undefined;
 }
 
-function forPropertyInObject(object, callback) {
-    var property;
+/**
+ * Loop over an `Object`.
+ *
+ * @param {Object.<string, *>} object
+ * @param {function(string, *)} callback
+ */
 
-    for (property in object) {
-        if (has(object, property)) {
-            callback(property, object[property]);
+function forPropertyInObject(object, callback) {
+    var key;
+
+    for (key in object) {
+        if (has(object, key)) {
+            callback(object[key], key);
         }
     }
 }
 
-function forValueInArray(array, callback) {
-    var iterator = -1,
-        length = array.length;
+/**
+ * Loop over an `Array`.
+ *
+ * @param {Array.<*>} array
+ * @param {function(*, number)} callback
+ */
 
-    while (++iterator < length) {
-        callback(array[iterator], iterator);
+function forValueInArray(array, callback) {
+    var index,
+        length;
+
+    index = -1;
+    length = array.length;
+
+    while (++index < length) {
+        callback(array[index], index);
     }
 }
 
-function add(object, property, value) {
-    object[property] = value;
-}
+/**
+ * Add all `values` to `object`.
+ *
+ * @param {Object.<string, *>} object
+ * @param {Object.<string, *>} values
+ */
 
 function addAll(object, values) {
-    forPropertyInObject(values, function (property, value) {
-        add(object, property, value);
+    forPropertyInObject(values, function (value, key) {
+        object[key] = value;
     });
 }
 
-function remove(object, property) {
-    object[property] = undefined;
-}
+/**
+ * Remove every key in `keys` from `object`.
+ *
+ * @param {Object.<string, *>} object
+ * @param {Array.<string>} keys
+ */
 
-function removeAll(object, array) {
-    forValueInArray(array, function (property) {
-        remove(object, property);
+function removeAll(object, keys) {
+    forValueInArray(keys, function (key) {
+        object[key] = undefined;
     });
 }
 
-function getProperty(object, property) {
-    return has(object, property) ? object[property] : null;
-}
+/**
+ * Add values to map.
+ *
+ * When the second argument is passed, it is treated as
+ * a single value and the first parameter as a key.
+ * Otherwise, every value in the first argument is added.
+ *
+ * @this DatamapInterface
+ * @param {Object.<string, *>|string} values
+ * @param {*} value
+ */
 
-datamapInterfacePrototype.add = function (values, value) {
+function add(values, value) {
     if (value) {
-        add(this.map, values, value);
+        this.map[values] = value;
     } else {
         addAll(this.map, values);
     }
-};
+}
 
-datamapInterfacePrototype.remove = function (values) {
-    (typeof values === 'string' ? remove : removeAll)(this.map, values);
-};
+/**
+ * Remove keys from map.
+ *
+ * When the second argument is passed, it is treated as
+ * a single value and the first parameter as a key.
+ * Otherwise, every value in the first argument is added.
+ *
+ * @this DatamapInterface
+ * @param {Array.<string>|string} keys
+ */
 
-datamapInterfacePrototype.all = function () {
-    var values = {};
+function remove(values) {
+    if (typeof values === 'string') {
+        this.map[values] = undefined;
+    } else {
+        removeAll(this.map, values);
+    }
+}
+
+/**
+ * Get all values.
+ *
+ * @this DatamapInterface
+ * @return {Object.<string, *>}
+ */
+
+function all() {
+    var values;
+
+    values = {};
 
     addAll(values, this.map);
 
     return values;
-};
+}
 
-datamapInterfacePrototype.get = function (property) {
-    return getProperty(this.map, property);
-};
+/**
+ * Get a value.
+ *
+ * @this DatamapInterface
+ * @param {string} key
+ * @return {*}
+ */
 
-datamapInterfacePrototype.has = function (property) {
-    return has(this.map, property);
-};
+function get(key) {
+    return has(this.map, key) ? this.map[key] : null;
+}
 
-exports = module.exports = DatamapInterface;
+/**
+ * Whether or not `value` is in context.
+ *
+ * @this DatamapInterface
+ * @param {*} value
+ * @return {boolean}
+ */
+
+function datamapHas(key) {
+    return has(this.map, key);
+}
+
+/**
+ * Expose methods on prototype.
+ */
+
+var datamapInterfacePrototype;
+
+datamapInterfacePrototype = DatamapInterface.prototype;
+
+datamapInterfacePrototype.add = add;
+
+datamapInterfacePrototype.remove = remove;
+
+datamapInterfacePrototype.all = all;
+
+datamapInterfacePrototype.get = get;
+
+datamapInterfacePrototype.has = datamapHas;
+
+/**
+ * Expose `DatamapInterface`.
+ */
+
+module.exports = DatamapInterface;
